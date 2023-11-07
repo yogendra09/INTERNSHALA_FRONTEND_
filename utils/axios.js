724a88f94c1669e369cf43f3620578fc7da7790a
yogendra singh <91890064+yogendra09@users.noreply.github.com>
2023-11-07 16:03:46 +0530

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"http://localhost:8080/",
+    baseURL:"https://internshala-8a7j.onrender.com/",
     withCredentials:true,
 
 });
